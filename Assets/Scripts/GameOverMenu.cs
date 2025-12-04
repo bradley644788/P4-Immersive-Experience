21d2e0939f38f093283831037af9425efedbfad9
Bradley Khan <644788@eastridingcollege.ac.uk>
2025-12-04 22:27:09 +0000

    capsule mesh hidden

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -9,21 +9,13 @@ public class GameOverMenu : MonoBehaviour
     public Transform monster;
     public float headOffsetY = 1.5f;
     public float lookSpeed = 2f;
-    // public Light torchLight;
-    // private float originalTorchIntensity;  
 
     public void ShowGameOver()  
     {  
         gameOverUI.SetActive(true);  
 
         Cursor.lockState = CursorLockMode.None;  
-        Cursor.visible = true;  
-
-        // if (torchLight != null)  
-        // {  
-        //     originalTorchIntensity = torchLight.intensity;  
-        //     StartCoroutine(SmoothDimTorch());  
-        // }  
+        Cursor.visible = true;
 
         StartCoroutine(SmoothLookAtMonster());  
         StartCoroutine(FreezeTimeNextFrame());  
@@ -45,20 +37,6 @@ public class GameOverMenu : MonoBehaviour
         playerCamera.transform.rotation = targetRotation;  
     }  
 
-    // private IEnumerator SmoothDimTorch()  
-    // {  
-    //     float targetIntensity = originalTorchIntensity * 0.05f;  
-    //     float startIntensity = torchLight.intensity;  
-    //     float t = 0f;  
-    //     while (t < 1f)  
-    //     {  
-    //         t += Time.unscaledDeltaTime * lookSpeed;  
-    //         torchLight.intensity = Mathf.Lerp(startIntensity, targetIntensity, t);  
-    //         yield return null;  
-    //     }  
-    //     torchLight.intensity = targetIntensity;  
-    // }  
-
     private IEnumerator FreezeTimeNextFrame()  
     {  
         yield return null;  
@@ -66,17 +44,13 @@ public class GameOverMenu : MonoBehaviour
     }  
 
     public void Retry()  
-    {  
-        // if (torchLight != null) torchLight.intensity = originalTorchIntensity;  
-
+    {
         Time.timeScale = 1f;  
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);  
     }  
 
     public void BackToMenu()  
-    {  
-        // if (torchLight != null) torchLight.intensity = originalTorchIntensity;  
-
+    {
         Time.timeScale = 1f;  
         SceneManager.LoadScene("StartMenu");  
     }
